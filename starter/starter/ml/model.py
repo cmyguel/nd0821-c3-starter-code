3bf11f4accf2caa6ca4909a317b517aada49372d
cristian_del_toro <cristiandltro@gmail.com>
2022-01-22 13:58:12 -0500

    single Inference Artifact

--- starter/starter/ml/model.py
+++ starter/starter/ml/model.py
@@ -1,6 +1,9 @@
 from sklearn.metrics import fbeta_score, precision_score, recall_score
 from sklearn.ensemble import RandomForestClassifier
-
+from pathlib import Path
+import pickle
+import pandas as pd
+from .data import process_data
 
 # Optional: implement hyperparameter tuning.
 def train_model(X_train, y_train):
@@ -60,3 +63,23 @@ def inference(model, X):
         Predictions from the model.
     """
     return model.predict(X)
+
+class Inference_artifact:
+    def __init__(self, model_path: Path):
+        with open( model_path, 'rb') as f:
+            (self.clf, self.encoder, self.lb, self.cat_features) = pickle.load(f)
+    
+    def predict(self, data: pd.DataFrame):
+        if 'salary' in data:
+            data = data.copy()
+            data.pop('salary')
+        X, _, _, _ = process_data(
+            data,
+            categorical_features=self.cat_features, 
+            label=None, 
+            training=False, 
+            encoder=self.encoder, 
+            lb=self.lb, 
+        )
+        pred = inference(self.clf, X)
+        return pred
