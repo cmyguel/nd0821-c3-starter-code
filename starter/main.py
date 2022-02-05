6ceb39c47df104d5da09d4323c847181d1346493
cristian del toro <cristiandltro@gmail.com>
2022-02-05 18:11:52 -0500

    predict RequestModel

--- starter/main.py
+++ starter/main.py
@@ -1,15 +1,11 @@
 # Put the code for your API here.
 import pandas as pd
-import pickle
-from fastapi import FastAPI, Request
-from typing import Union, List
+from fastapi import FastAPI
+from typing import Union, Dict
 from pydantic import BaseModel, Field
 from pathlib import Path
 
-from starter.ml.data import process_data
 from starter.ml.model import Inference_artifact
-
-
 
 # instantiate the app
 app = FastAPI()
@@ -23,41 +19,32 @@ async def say_hello():
     return {"greetings": "Welcome to the >50k maker detection API."}
 
 class RequestModel(BaseModel):
-    age: dict
-    workclass: dict
-    fnlgt: dict
-    education: dict
-    education_num: dict = Field(alias='education-num')
-    marital_status: dict = Field(alias='marital-status')
-    occupation: dict
-    relationship: dict
-    race: dict
-    sex: dict
-    capital_gain: dict = Field(alias='capital-gain')
-    capital_loss: dict = Field(alias='capital-loss')
-    hours_per_week: dict = Field(alias='hours-per-week')
-    native_country: dict = Field(alias='native-country')
-    salary: dict
+    age:            Union[int, Dict[str, int]]
+    workclass:      Union[str, Dict[str, str]]
+    fnlgt:          Union[int, Dict[str, int]]
+    education:      Union[str, Dict[str, str]]
+    occupation:     Union[str, Dict[str, str]]
+    relationship:   Union[str, Dict[str, str]]
+    race:           Union[str, Dict[str, str]]
+    sex:            Union[str, Dict[str, str]]
+    salary:         Union[str, Dict[str, str]]
+    education_num:  Union[int, Dict[str, int]] = Field(alias='education-num')
+    marital_status: Union[str, Dict[str, str]] = Field(alias='marital-status')
+    capital_gain:   Union[int, Dict[str, int]] = Field(alias='capital-gain')
+    capital_loss:   Union[int, Dict[str, int]] = Field(alias='capital-loss')
+    hours_per_week: Union[int, Dict[str, int]] = Field(alias='hours-per-week')
+    native_country: Union[str, Dict[str, str]] = Field(alias='native-country')
 
 @app.post('/')
 async def predict(request: RequestModel):
-    # input_data = await request.json()
+    
     input_data = request.dict()
 
-    # change underlines by dashes in keys from input_data keys 
+    # change underlines in keys from input_data keys 
     # (_) -> (-)
     for k in list(input_data.keys()):
         if "_" in k:
             input_data[k.replace('_', '-')] = input_data.pop(k)
-
-    print(50*"-")
-    print("input_data.keys():")
-    print(input_data.keys())
-
-    print(50*"-")
-    print("types:")
-    for key,val in input_data.items():
-        print(key, type(val), type([v for v in val.values()][0]))
 
     input_df = pd.DataFrame(input_data)
     if 'salary' in input_df:
@@ -65,16 +52,3 @@ async def predict(request: RequestModel):
     
     pred = inference.predict(input_df)
     return {"prediction": pred.tolist()}
-
-
-# @app.post('/')
-# async def predict(request: Request):
-#     input_data = await request.json()
-
-#     input_df = pd.DataFrame(input_data)
-#     if 'salary' in input_df:
-#         input_df.pop('salary')
-    
-#     pred = inference.predict(input_df)
-
-#     return {"prediction": pred.tolist()}
