3ffb855fe3ffbffd6e6246f14ceed304aca816ec
cristian del toro <cristiandltro@gmail.com>
2022-02-08 08:39:04 -0500

    unimportant: refactor

--- starter/main.py
+++ starter/main.py
@@ -4,7 +4,6 @@ from fastapi import FastAPI
 from typing import Union, Dict, Optional
 from pydantic import BaseModel, Field
 from pathlib import Path
-
 from starter.ml.model import Inference_artifact
 
 # instantiate the app
@@ -14,13 +13,8 @@ FOLDER_MODEL = Path("model")
 inference = Inference_artifact(
                                 FOLDER_MODEL/'model.pkl',
                                 FOLDER_MODEL/'onehot_encoder.pkl',
-                                ) 
-
-# GET welcome message
-@app.get('/')
-async def say_hello():
-    return {"greetings": "Welcome to the >50k maker detection API."}
-
+                                )
+# request model for post /predict
 class RequestModel(BaseModel):
     age:            Union[int, Dict[str, int]]
     workclass:      Union[str, Dict[str, str]]
@@ -38,11 +32,15 @@ class RequestModel(BaseModel):
     hours_per_week: Union[int, Dict[str, int]] = Field(alias='hours-per-week')
     native_country: Union[str, Dict[str, str]] = Field(alias='native-country')
 
+# GET welcome message
+@app.get('/')
+async def say_hello():
+    return {"greetings": "Welcome to the >50k maker detection API."}
+
+# POST make prediction
 @app.post('/predict')
 async def predict(request: RequestModel):
-    
     input_data = request.dict()
-
     # change underlines in keys from input_data keys 
     # (_) -> (-)
     for k in list(input_data.keys()):
@@ -55,6 +53,7 @@ async def predict(request: RequestModel):
     else: 
         input_df = pd.DataFrame(input_data, index=[0])
 
+    # remove target column from DataFrame
     if 'salary' in input_df:
         input_df.pop('salary')
     
